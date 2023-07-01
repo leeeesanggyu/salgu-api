f2c489f8c81f4b5da8d9f95d536aad773af6576d
salgu1998 <salgu1998@gmail.com>
2023-07-01 21:48:00 +0900

    project setting

---
+++ src/main/java/com/salgu/salguapi/SalguApiApplication.java
@@ -0,0 +1,13 @@
+package com.salgu.salguapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalguApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalguApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salgu/salguapi/SalguApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salgu.salguapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalguApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
