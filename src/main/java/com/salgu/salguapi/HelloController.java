b193f858abb5cfd81ce6485dcb0ca1693187f3af
salgu1998 <salgu1998@gmail.com>
2023-07-02 00:18:15 +0900

    profile setting

---
+++ src/main/java/com/salgu/salguapi/HelloController.java
@@ -0,0 +1,17 @@
+package com.salgu.salguapi;
+
+import com.salgu.salguapi.util.response.ResponseWithData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseWithData hello() {
+        log.info("HelloController.hello()");
+        return ResponseWithData.success();
+    }
+}
