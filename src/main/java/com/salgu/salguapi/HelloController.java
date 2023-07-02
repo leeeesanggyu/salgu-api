88b56b756c0b1fb5353a12361f6a1de7918e8d5d
salgu1998 <salgu1998@gmail.com>
2023-07-02 13:58:51 +0900

    dockerized

--- src/main/java/com/salgu/salguapi/HelloController.java
+++ src/main/java/com/salgu/salguapi/HelloController.java
@@ -2,6 +2,7 @@ package com.salgu.salguapi;
 
 import com.salgu.salguapi.util.response.ResponseWithData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${profile.message}")
+    private String message;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @GetMapping("/hello")
     public ResponseWithData hello() {
-        log.info("HelloController.hello()");
-        return ResponseWithData.success();
+        log.info("HelloController.hello(), {}, {}", message, dbUrl);
+        return ResponseWithData.success(message);
     }
 }
