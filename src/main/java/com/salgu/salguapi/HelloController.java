c78ef9a809f97defea1c805349f42ffc438dab3e
salgu1998 <salgu1998@gmail.com>
2023-07-19 15:23:19 +0900

    application status check, change api 구현

--- src/main/java/com/salgu/salguapi/HelloController.java
+++ src/main/java/com/salgu/salguapi/HelloController.java
@@ -3,7 +3,10 @@ package com.salgu.salguapi;
 import com.salgu.salguapi.util.response.ResponseWithData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,9 +19,31 @@ public class HelloController {
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
+    private Boolean applicationStatus = true;
+
     @GetMapping("/hello")
     public ResponseWithData hello() {
         log.info("HelloController.hello(), {}, {}", message, dbUrl);
         return ResponseWithData.success(message);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity getStatus() {
+        if (applicationStatus == false) {
+            return new ResponseEntity<>("now => " + applicationStatus, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>("now => " + applicationStatus, HttpStatus.OK);
+    }
+
+    @PatchMapping("/status")
+    public ResponseEntity changeStatus() {
+        if (applicationStatus == true) {
+            applicationStatus = false;
+        } else if (applicationStatus == false) {
+            applicationStatus = true;
+        }
+
+        return new ResponseEntity<>("now => " + applicationStatus, HttpStatus.OK);
     }
 }
